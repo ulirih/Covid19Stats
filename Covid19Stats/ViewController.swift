e5b34d9edde92bf6957b8f38ef5871be0a2766e0
perevedniuk <perevedniuk@gmail.com>
2022-05-02 21:44:13 +0300

    Initial Commit

---
+++ Covid19Stats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid19Stats
+//
+//  Created by andrey perevedniuk on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
