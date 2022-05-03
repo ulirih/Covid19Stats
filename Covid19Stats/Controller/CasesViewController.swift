2fc91427b054cd7b5c05522d163b396edccbcacd
perevedniuk <perevedniuk@gmail.com>
2022-05-03 11:15:20 +0300

    init

---
+++ Covid19Stats/Controller/CasesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CasesViewController.swift
+//  Covid19Stats
+//
+//  Created by andrey perevedniuk on 02.05.2022.
+//
+
+import UIKit
+
+class CasesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Covid19Stats/Controller/VaccinesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VaccinesViewController.swift
+//  Covid19Stats
+//
+//  Created by andrey perevedniuk on 02.05.2022.
+//
+
+import UIKit
+
+class VaccinesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
